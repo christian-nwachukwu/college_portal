14f0a60b1380e58bd7c16b4e16f3a45224f1a6f1
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2024-08-19 14:35:20 +0100

    Created Register.html, its urls.py, models.py, forms.py, admin.py and database migrations. Also created records in the Django admin backend.

--- portal/urls.py
+++ portal/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),    #Here, create a path to call the view.py by importing views. Then Goto views to create the function to be rendered.
     #path('login/', views.login_user, name='login'),
-    #path('logout/', views.logout_user, name='logout'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
 # Home page
 def index(request):
-    #   check to confirm login
+    #   check to confirm login username and password
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        #   authenticate
+        #   authenticate user
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -24,5 +26,30 @@ def index(request):
         return render(request, 'index.html', {})    # Render the html files. Add the template folder to setting.py. Create html files in templates folder
 
 
+
+#   Create Logout logic
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('index')
+
+
+#   create register logic
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #   lets authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration Successful!')
+            return redirect('index')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
